da451a669029eaee6f52ea0fa56cadbb00cfe4ee
Matúš Suský <matussusky@gmail.com>
2023-06-07 12:58:24 +0200

    Add project files.

---
+++ FileStatisticFilter.CommonLibrary/SearchedFile.cs
@@ -0,0 +1,147 @@
+﻿using System.Text;
+
+namespace FileStatisticFilter.CommonLibrary
+{
+    public class SearchedFile
+    {
+        public DateTime CreatedTime { get; set; }
+        public string Directory { get; set; }
+        public string Extension { get; set; }
+        public string FileName => FileNameWithoutExtension + Extension;
+        public string FileNameWithoutExtension { get; set; }
+        public string FullName => Directory + FileNameWithoutExtension + Extension;
+        public bool IsReadOnly { get; set; }
+        public DateTime ModifiedTime { get; set; }
+        public long Size { get; set; }
+
+        public string SizeReadable
+        {
+            get
+            {
+                string[] sizes = { "B", "KB", "MB", "GB", "TB" };
+                long tmp = Size;
+                int size = 0;
+                string returnSize;
+                while (tmp > 0)
+                {
+                    tmp = tmp / 1000;
+                    size++;
+                }
+                returnSize = (tmp * 1000) + sizes[size];
+                return returnSize;
+            }
+        }
+
+        public SearchedFile(FileInfo file)
+        {
+            CreatedTime = file.CreationTime;
+            Directory = file.DirectoryName;
+            Extension = file.Extension;
+
+            var name = file.Name;
+            name = name.Substring(0, name.Length - Extension.Length);
+
+            FileNameWithoutExtension = name;
+            IsReadOnly = file.IsReadOnly;
+            ModifiedTime = file.LastWriteTime;
+            Size = file.Length;
+        }
+
+        public SearchedFile(string csvLine, char delimiter = '\t')
+        {
+            var splitted = csvLine.Split(delimiter);
+
+            DateTime ct;
+            DateTime.TryParse(splitted[4], out ct);
+            CreatedTime = ct;
+
+            Directory = splitted[0];
+            Extension = splitted[2];
+            FileNameWithoutExtension = splitted[1];
+
+            bool b;
+            bool.TryParse(splitted[6], out b);
+            IsReadOnly = b;
+
+            DateTime mt;
+            DateTime.TryParse(splitted[5], out mt);
+            ModifiedTime = mt;
+
+            long size;
+            long.TryParse(splitted[3], out size);
+            Size = size;
+        }
+
+        public static string ToCsvHeaderLine(char delimiter = '\t')
+        {
+            return "Directory" + delimiter + "FileNameWithoutExtension" + delimiter + 
+                   "Extension" + delimiter + "Size" + delimiter + 
+                   "CreatedTime" + delimiter + "ModifiedTime" + delimiter + "IsReadOnly";
+        }
+
+        public string ToCsvLine(char delimiter = '\t')
+        {
+            return Directory + delimiter + FileNameWithoutExtension + delimiter +
+                   Extension + delimiter + Size + delimiter +
+                   CreatedTime + delimiter + ModifiedTime + delimiter + IsReadOnly;
+        }
+    }
+
+    public class SearchedFiles
+    {
+        public SearchedFile[] Files { get; set; }
+
+        public SearchedFiles()
+        {
+        }
+
+        public SearchedFiles(IEnumerable<FileInfo> files)
+        {
+            var tmpFiles = new List<SearchedFile>();
+            foreach (var file in files)
+            {
+                tmpFiles.Add(new SearchedFile(file));
+            }
+
+            Files = tmpFiles.ToArray();
+        }
+
+        public void LoadFromCsv(FileInfo file)
+        {
+            var stream = file.Open(FileMode.Open);
+            using var reader = new StreamReader(stream);
+            var tmpFiles = new List<SearchedFile>();
+            var header = true;
+            while (!reader.EndOfStream)
+            {
+                var line = reader.ReadLine();
+                if (!header)
+                {
+                    tmpFiles.Add(new SearchedFile(line));
+                }
+                else
+                {
+                    header = false;
+                }
+            }
+
+            Files = tmpFiles.ToArray();
+            reader.Close();
+            stream.Close();
+        }
+
+        public void SaveToCsv(FileInfo file)
+        {
+            var stream = file.Open(FileMode.Create);
+            using var writer = new StreamWriter(stream);
+            writer.WriteLine(SearchedFile.ToCsvHeaderLine(';'));
+            for (int i = 0; i < Files.Length; i++)
+            {
+                writer.WriteLine(Files[i].ToCsvLine(';'));
+            }
+            writer.Flush();
+            writer.Close();
+            stream.Close();
+        }
+    }
+}
---
+++ FileStatisticFilter.SearchConsoleApp/Launcher.cs
@@ -0,0 +1,31 @@
+﻿using FileStatisticFilter.CommonLibrary;
+
+namespace FileStatisticFilter.SearchConsoleApp
+{
+    internal class Launcher
+    {
+        /// <summary>
+        /// File analyzer
+        /// </summary>
+        /// <param name="input">Required parameter, path to directory to be analyzed</param>
+        /// <param name="output">Required parameter, CSV file for saving information about files</param>
+        /// <param name="regex">NETUSIM</param>
+        /// <param name="recursive">If entered, the program wil' also search subfolders</param>
+        static void Main(string input, string output, string regex = "\t", bool recursive = false)
+        {
+            var directory = new DirectoryInfo(input);
+            SearchedFiles files;
+            if (!directory.Exists)
+            {
+                Console.Error.WriteLine("Entered input file does not exists!");
+            }
+            else
+            {
+                files = recursive 
+                    ? new SearchedFiles(directory.GetFiles("*", SearchOption.AllDirectories)) 
+                    : new SearchedFiles(directory.GetFiles("*", SearchOption.TopDirectoryOnly));
+                files.SaveToCsv(new FileInfo(output));
+            }
+        }
+    }
+}
